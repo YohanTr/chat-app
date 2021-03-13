7ae313736478b80456339a36ee591fba9b102f46
Yohan Tahir <yohantahir@gmail.com>
2021-03-13 20:51:36 +0100

    create class Contact with state for online or offline

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,32 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 import "../components/Contact.css";
 
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.avatar} alt="women" />
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: true
+    };
+  }
+
+  render () {
+    return <div className="Contact">
+      <img className="avatar" src={this.props.avatar} alt="women" />
       <div className="status">
-        <p className="name"><strong>{props.name}</strong></p>
-        <p className='status-text'><span className={props.online ? 'status-online' : 'status-offline'}></span>{props.online ? 'Online' : 'Offline'}</p>
+        <p className="name"><strong>{this.props.name}</strong></p>
+        <p className='status-text' onClick={(event) => {
+          const isOnline = !this.state.online;
+          this.setState({ online: isOnline});
+        }}><span className={this.state.online ? 'status-online' : 'status-offline'}></span>{this.state.online ? 'Online' : 'Offline'}</p>
       </div>
     </div>
-  );
+  }
 }
 
 Contact.propTypes = {
   name: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  online: PropTypes.bool.isRequired
 };
 export default Contact;
